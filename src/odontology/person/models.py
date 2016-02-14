97fa6dfdc8dc1498dbf2da6af11bdfb3bc5f1f9c
nanomolina <nanomolinacav@gmail.com>
2016-02-14 06:25:02 -0300

    Detail apross + extension de modelo paciente

--- src/odontology/person/models.py
+++ src/odontology/person/models.py
@@ -17,12 +17,37 @@ class Dentist(models.Model):
     register_number = models.IntegerField()
 
 
+GENDER = (
+    (1, 'M'),
+    (2, 'F')
+)
+
 class Patient(models.Model):
     dentist = models.ForeignKey(Dentist, null=True, blank=True) #sacar el null
     clinic_history = models.OneToOneField(Clinic_history, null=True, blank=True)
     first_name = models.CharField(max_length=250)
     last_name = models.CharField(max_length=250)
     subsidiary_number = models.PositiveIntegerField("numero de afiliado", unique=True)
+
+    #-- extra info
+    incumbent = models.CharField(max_length=250, null=True, blank=True)
+    family_group = models.CharField(max_length=250, null=True, blank=True)
+    relationship = models.CharField(max_length=250, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    street = models.CharField(max_length=250, null=True, blank=True)
+    number = models.PositiveIntegerField(null=True, blank=True)
+    suburb = models.CharField(max_length=250, null=True, blank=True)
+    locality = models.CharField(max_length=250, null=True, blank=True)
+    tel = models.IntegerField(null=True, blank=True)
+    Workplace_holder = models.CharField(max_length=250, null=True, blank=True)
+    hierarchy = models.CharField(max_length=250, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    gender = models.IntegerField(choices=GENDER, null=True, blank=True)
+
+
+    @property
+    def domicile(self):
+        return self.street + ', ' + str(self.number) + ', ' + self.suburb + ', ' + self.locality
 
     class Meta:
         verbose_name = "Paciente"
