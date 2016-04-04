dbd272814a481def2273e70143f0ab8d4b38bccf
nanomolina <nanomolinacav@gmail.com>
2016-04-04 02:26:57 -0300

    se añade actions de admin

--- src/odontology/register/admin.py
+++ src/odontology/register/admin.py
@@ -2,11 +2,23 @@ from django.contrib import admin
 from register.models import Apross, DetailApross, Faces, Benefit, DetailBenefit, Radiography
 
 
+def apross_create_radiography(modeladmin, request, queryset):
+    for bf in queryset:
+        Radiography(apross=bf).save()
+apross_create_radiography.short_description = "Crear radiografia"
+
+def benefit_create_radiography(modeladmin, request, queryset):
+    for bf in queryset:
+        Radiography(benefit=bf).save()
+benefit_create_radiography.short_description = "Crear radiografia"
+
+
 class AprossAdmin(admin.ModelAdmin):
     fields = (
         'patient', 'month', 'year', 'managment_code1',
         'managment_code2', 'managment_code3', 'rx_amount',
     )
+    actions = [apross_create_radiography]
 admin.site.register(Apross, AprossAdmin)
 
 
@@ -23,6 +35,7 @@ class BenefitAdmin(admin.ModelAdmin):
         'patient', 'month', 'year', 'primary_entity',
         'principal_code', 'managment_code', 'rx_amount',
     )
+    actions = [benefit_create_radiography]
 admin.site.register(Benefit, BenefitAdmin)
 
 
