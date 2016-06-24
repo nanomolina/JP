4dfb8bbbbb2a190ea38822f42344d177bb00bc0e
Nano Molina <nanomolinacav@gmail.com>
2016-06-24 11:44:17 -0300

    profile image

---
+++ src/odontology/core/templatetags/core_extras.py
@@ -0,0 +1,10 @@
+from django import template
+register = template.Library()
+
+@register.filter(name='get_avatar_url')
+def get_avatar_url(user):
+    if user.socialaccount_set.exists():
+        avatar = user.socialaccount_set.first().get_avatar_url()
+    else:
+        avatar = None
+    return avatar
