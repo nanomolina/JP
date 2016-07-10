d91e924dac70746ac608f0a24cbac15e009088fa
nanomolina <nanomolinacav@gmail.com>
2016-07-10 04:03:45 -0300

    JP-033 nuevas campos de paciente agragados

--- src/odontology/core/admin.py
+++ src/odontology/core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from core.models import Bill
+from core.models import Bill, Day
+
+class DayAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Day, DayAdmin)
 
 class BillAdmin(admin.ModelAdmin):
     fields = ('user', 'paid', 'url_file', 'text')
