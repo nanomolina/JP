525cd08e0f92ad32a891fe49999abe50d14b3604
nanomolina <nanomolinacav@gmail.com>
2016-07-10 23:51:07 -0300

    JP-044 envio de mail y templates de cambio de contraseña

--- src/odontology/odontology/local_settings.py
+++ src/odontology/odontology/local_settings.py
@@ -161,3 +161,9 @@ SOCIALACCOUNT_PROVIDERS = \
     { 'google':
         { 'SCOPE': ['profile', 'email'],
           'AUTH_PARAMS': { 'access_type': 'online'} }}
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = DEFAULT_FROM_EMAIL = ''
+EMAIL_HOST_PASSWORD = ''
