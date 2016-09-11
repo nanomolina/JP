a10f7acaaa6761c4d4975690d8ac7f05f07e27e9
Nano Molina <nanomolinacav@gmail.com>
2016-09-11 12:17:57 -0300

    Update server_settings.py

--- src/odontology/odontology/server_settings.py
+++ src/odontology/odontology/server_settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h0_%$7qsz@i-^7_&tcl0_xayt1ce46&20d2#xw=jhvn+gfpm45'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.dentalsoft.com.ar']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
